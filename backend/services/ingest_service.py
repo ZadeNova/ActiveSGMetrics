bc53df621346c7ff05a727d73bbaf2a32fc7d896
ZadeNova <muhderfannnn@gmail.com>
2026-01-21 12:05:33 +0800

    Fixed a bug in ingest_service.py

--- backend/services/ingest_service.py
+++ backend/services/ingest_service.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 from models import GymMetaData , GymOccupancyData
 
 def ingest_gym_data(payload, db):
@@ -7,34 +8,32 @@ def ingest_gym_data(payload, db):
     batch_time = datetime.now(timezone.utc).replace(second=0, microsecond=0)
     
     try:
-        existing_gym_ids = set(db.exec(select(GymMetaData.facility_id)).all())
-        occupancy_records = []
+        
                 
-        for item in payload:
+        if payload:
+            metadata_list = [
+                {"facility_id": item.id, "name": item.name, "facility_type": item.type}
+                for item in payload
+            ]        
+        
+            stmt = insert(GymMetaData).values(metadata_list)
+            upsert_stmt = stmt.on_conflict_do_nothing(index_elements=["facility_id"])
+            db.exec(upsert_stmt)
+        
+        
             # Check the gym metadata table first before uploading the gym occupancy data
             
+
+                
+        occupancy_records = [
             
-            if item.id not in existing_gym_ids:
-                # If there is a new gym added, add it to the gym metadata table
-                
-                new_metadata = GymMetaData(
-                    facility_id= item.id,
-                    name= item.name,
-                    facility_type=item.type
-                )
-                
-                db.add(new_metadata)
-                existing_gym_ids.add(item.id)
-                
-            # Start recording the gym occupancy data into the table. INSERT operation
-            occupancy_records.append(
-              GymOccupancyData(
-                facility_id= item.id,
-                occupancy_percentage= item.capacityPercentage,
+            GymOccupancyData(
+                facility_id=item.id,
+                occupancy_percentage=item.capacityPercentage,
                 is_closed=item.isClosed,
                 timestamp=batch_time
-            )
-              )
+            ) for item in payload
+        ]
             
         
         
