bdbcdc49ebdc00cf3c5f6fbff3a53dcad17d9dc5
ZadeNova <muhderfannnn@gmail.com>
2026-03-11 01:27:48 +0800

    Make the config file optional.

--- backend/config.py
+++ backend/config.py
@@ -1,17 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
 
 class Settings(BaseSettings):
+    # Strictly required
     SUPABASE_DATABASE_URL: str
     ACTIVE_SG_API: str
     
-    SUPABASE_DEV_DATABASE_URL: str
-    WEBSITE_URL: str
+    # Optional
+    SUPABASE_DEV_DATABASE_URL: Optional[str] = None
+    WEBSITE_URL: Optional[str] = None
     
-    LOCAL_BACKEND_URL: str
-    LOCAL_BACKEND_HEALTH_URL: str
+    LOCAL_BACKEND_URL: str = "http://localhost:8000"
+    LOCAL_BACKEND_HEALTH_URL: Optional[str] = None
     
-    PROD_BACKEND_URL: str
+    PROD_BACKEND_URL: Optional[str] = None
     
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
