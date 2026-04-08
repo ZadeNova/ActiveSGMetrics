750729613376019430f0a381dd96e59561aec883
ZadeNova <muhderfannnn@gmail.com>
2026-04-09 03:13:34 +0800

    deleted main.py include_router for ingest.router

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,6 @@ app.add_middleware(
 
 
 #app.include_router(data_ingest_router, prefix="/api/v1", tags=["Data_Ingestion"])
-app.include_router(ingest.router, prefix="/api/v1", tags=["Ingest_Data"])
+#app.include_router(ingest.router, prefix="/api/v1", tags=["Ingest_Data"])
 app.include_router(data.router, prefix="/api/v1", tags=["Data"])
 app.include_router(health.router, prefix="/api/v1", tags=["Health"])
